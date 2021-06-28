8a98bda0e15269f91fda2ad8072a8083a2d5dd7f
Steve Saylor <steven.saylor@pitt.edu>
2021-06-28 13:12:57 -0400

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const path = require('path');
+const withTM = require('next-transpile-modules')(['../.',]); // pass the modules you would like to see transpiled
+
+
+module.exports = withTM({
+  webpack: (config, options) => {
+    if (options.isServer) {
+      config.externals = ['react', ...config.externals];
+    }
+
+    config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
+
+    return config
+  },
+});
