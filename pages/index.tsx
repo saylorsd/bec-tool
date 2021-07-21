998329f9f18df50265600e502ac31159fb3029b7
Steve Saylor <steven.saylor@pitt.edu>
2021-07-21 11:46:43 -0400

    updated components to 0.1.4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+// noinspection SqlResolve
+
 import {useState} from 'react';
 import '@wprdc/test-components/src/styles/global.css'
 
@@ -8,7 +10,7 @@ import {
   DataVizVariant,
   Divider,
   GeogIdentifier,
-  GeographyType,
+  GeographyType, GeogTypeDescriptor,
   LayerPanelVariant,
   Map,
   MapProps
@@ -25,6 +27,14 @@ export default function Home() {
     vizType: DataVizType.BigValue,
     description: '',
     staticOptions: {},
+  };
+  const dataViz2 = {
+    "id": 34,
+    "name": "Health Insurance Coverage",
+    "slug": "health-insurance-coverage-table",
+    "vizType": DataVizType.Table,
+    "description": "Number and percent of civilian non-institutionalized population with health insurance coverage",
+    "staticOptions": {},
   };
 
   const assetTypes = [
@@ -74,13 +84,13 @@ export default function Home() {
       <div className="flex-grow flex-shrink border-r border-black">
         <Map
           mapboxApiAccessToken="pk.eyJ1Ijoic3RldmVuZHNheWxvciIsImEiOiJja295ZmxndGEwbGxvMm5xdTc3M2MwZ2xkIn0.WDBLMZYfh-ZGFjmwO82xvw"
-          layerPanelVariant={LayerPanelVariant.Inside}
+          layerPanelVariant={LayerPanelVariant.Left}
           assetTypes={assetTypes}
-          menuGeogType={GeographyType.BlockGroup}
+          menuGeogTypes={menuLayers}
           onClick={handleClick}
         />
       </div>
-      <div className="w-96 p-2 flex-column">
+      <div className="min-w-0 w-1/4 p-2 flex-column">
         <div className="min-h-0 max-h-24">
           <ConnectedGeographySection {...geog} />
           <Divider weight="thick"/>
@@ -90,7 +100,14 @@ export default function Home() {
             <ConnectedDataViz
               variant={DataVizVariant.Blurb}
               geogIdentifier={geog}
-              dataVizID={dataViz}
+              dataVizID={bigValViz}
+            />
+          )}
+          {!!geog && (
+            <ConnectedDataViz
+              variant={DataVizVariant.Details}
+              geogIdentifier={geog}
+              dataVizID={chartViz}
             />
           )}
         </div>
@@ -98,3 +115,51 @@ export default function Home() {
     </div>
   );
 }
+
+const menuLayers: GeogTypeDescriptor[] = [
+  {
+    id: GeographyType.BlockGroup,
+    name: 'Block Group',
+    tableName: 'census_blockgroup',
+    cartoSql:
+      "SELECT *, name as map_name, 'blockGroup' as geogType, geoid as geogID FROM census_blockgroup  WHERE statefp = '42' AND countyfp IN ('003','019','128','007','005','063','129','051','059','125','073')",
+    description: 'Smallest geographical unit w/ ACS sample data.',
+  },
+  {
+    id: GeographyType.Tract,
+    name: 'Tract',
+    tableName: 'census_tract',
+    cartoSql:
+      "SELECT *, name as map_name, 'tract' as geogType, geoid as geogID FROM census_tract  WHERE statefp = '42' AND countyfp IN ('003','019','128','007','005','063','129','051','059','125','073')",
+    description: 'Drawn to encompass ~2500-8000 people',
+  },
+];
+
+const bigValViz = {
+  id: 39,
+  name: 'Median Age',
+  slug: 'median-age',
+  vizType: DataVizType.BigValue,
+  description: '',
+  staticOptions: {},
+};
+
+const tableViz = {
+  id: 16,
+  name: 'Population by Race',
+  slug: 'pop-by-race',
+  vizType: DataVizType.Table,
+  description: '',
+  staticOptions: {},
+};
+
+const chartViz = {
+  id: 68,
+  name: 'Population by Race',
+  slug: 'pop-race-bar-chart',
+  vizType: DataVizType.Chart,
+  description: '',
+  staticOptions: {
+    acrossGeogs: false,
+  },
+};
